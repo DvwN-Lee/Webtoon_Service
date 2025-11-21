2b1884da715c97a8be68d2961bf9f4e20e5369e9
[CLOUD4] 유연주 <ccomi0122@gmail.com>
2025-11-21 18:05:37 +0900

    refactor: EpisodeStats.java, Webtoon.java 타입 Long으로 수정

--- webtoon-rental-system/src/main/java/com/webtoon/domain/EpisodeStats.java
+++ webtoon-rental-system/src/main/java/com/webtoon/domain/EpisodeStats.java
@@ -6,13 +6,13 @@ package com.webtoon.domain;
  */
 public class EpisodeStats {
 
-    private final String episodeId;
-    private final String webtoonId;
+    private final Long episodeId;
+    private final Long webtoonId;
     private final int episodeNumber;
     private final int viewCount;
 
-    public EpisodeStats(String episodeId,
-                        String webtoonId,
+    public EpisodeStats(Long episodeId,
+                        Long webtoonId,
                         int episodeNumber,
                         int viewCount) {
         this.episodeId = episodeId;
@@ -21,11 +21,11 @@ public class EpisodeStats {
         this.viewCount = viewCount;
     }
 
-    public String getEpisodeId() {
+    public Long getEpisodeId() {
         return episodeId;
     }
 
-    public String getWebtoonId() {
+    public Long getWebtoonId() {
         return webtoonId;
     }
 
@@ -40,8 +40,8 @@ public class EpisodeStats {
     @Override
     public String toString() {
         return "EpisodeStats{" +
-                "episodeId='" + episodeId + '\'' +
-                ", webtoonId='" + webtoonId + '\'' +
+                "episodeId=" + episodeId +
+                ", webtoonId=" + webtoonId +
                 ", episodeNumber=" + episodeNumber +
                 ", viewCount=" + viewCount +
                 '}';
